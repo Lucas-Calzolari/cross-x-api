4f8cef45a08451e78cc10757266849df4b5d4065
Lucas Calzolari <lucascpds@gmail.com>
2022-06-04 18:01:18 -0300

    Refactor api into class

--- src/crossAPI.ts
+++ src/crossAPI.ts
@@ -1,32 +1,49 @@
-import axios from 'axios';
+import axios, { Axios } from 'axios';
 import { Dayjs } from 'dayjs';
 import ClassItem from './types/ClassItem';
-axios.defaults.baseURL = 'https://api-v2.appcrossx.com/api/1.0/app';
-axios.defaults.headers.common['User-Agent'] = 'okhttp/3.12.1'
-axios.defaults.headers.common['Accept'] = 'pt-BR'
-axios.defaults.headers.common['box-id'] = '3007'
-axios.defaults.headers.post['Content-Type'] = 'application/json'
 
-export const setToken = (token : string) => {
-    axios.defaults.headers.common['Authorization'] = token.toLowerCase().startsWith('bearer') ? token : `Bearer ${token}`;
-}
+const BASE_URL = 'https://api-v2.appcrossx.com/api/1.0/app';
+const HEADER_USER_AGENT = 'okhttp/3.12.1'
+const HEADER_ACCEPT = 'pt-BR'
+const HEADER_BOX_ID = '3007'
+const HEADER_CONTENT_TYPE = 'application/json'
 
-export const getClassesForDate = async (date : Dayjs) => {
-    const {headers, data, status} = await axios.get<{data : Array<ClassItem>, message : string}>(`/gym-classes?class_date=${date.format('YYYY-MM-DD')}`)
-    return data.data;
-}
+export class CrossAPI {
 
-export const checkin = async (classId : number) =>  {
+    axiosInstance!: Axios;
+
+    constructor() {
+        this.axiosInstance = axios.create({
+            baseURL :BASE_URL, 
+            headers : {
+                "User-Agent" :HEADER_USER_AGENT, 
+                'Accept' :HEADER_ACCEPT, 
+                'Content-Type' : HEADER_CONTENT_TYPE,
+                'box-id' : HEADER_BOX_ID
+            }
+        })
+    }
+    setToken = (token : string) => {
+        this.axiosInstance.defaults.headers.common['Authorization'] = token.toLowerCase().startsWith('bearer') ? token : `Bearer ${token}`;
+    }
     
-    console.log({classId})
-    try {
-        await axios.post('/checkin/make', {"gym_class_id" : classId})
-        return true;
-    } catch (e) {console.log("Error on checkin for class " + classId)}
-    return false
-}
-
-export const authenticate = async (email: string, password : string) => {
-    const {data, status} = await axios.post<{data : {user : any, token : string}}>('/auth/sign-in', {email, password, type : 1})
-    setToken(data.data.token)
+    getClassesForDate = async (date : Dayjs) => {
+        const {headers, data, status} = await this.axiosInstance.get<{data : Array<ClassItem>, message : string}>(`/gym-classes?class_date=${date.format('YYYY-MM-DD')}`)
+        return data.data;
+    }
+    
+    checkin = async (classId : number) =>  {
+        
+        console.log({classId})
+        try {
+            await this.axiosInstance.post('/checkin/make', {"gym_class_id" : classId})
+            return true;
+        } catch (e) {console.log("Error on checkin for class " + classId)}
+        return false
+    }
+    
+    authenticate = async (email: string, password : string) => {
+        const {data, status} = await this.axiosInstance.post<{data : {user : any, token : string}}>('/auth/sign-in', {email, password, type : 1})
+        this.setToken(data.data.token)
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,63 @@
 import dayjs from 'dayjs';
-import { authenticate, checkin, getClassesForDate, setToken } from './crossAPI'
+import { CrossAPI } from './crossAPI'
 import {ArgumentParser} from 'argparse' 
 import { exit } from 'process';
 
-export * from './crossAPI';
 
-console.log("Start process: " + dayjs().format("YYYY-MM-DDTHH:mm:ss:SSSZ"))
+// console.log("Start process: " + dayjs().format("YYYY-MM-DDTHH:mm:ss:SSSZ"))
 
-const parser = new ArgumentParser({
-    description: "CLI cross X checkin"
-});
-parser.add_argument('-e', '--email', { help: 'Email for login' });
-parser.add_argument('-p', '--password', { help: 'Password for login' });
-parser.add_argument('--token', { help: 'Directly use token instead of credentials' });
-parser.add_argument('-t', '--time', { help: 'Class time format HH:mm', required: true });
+// const parser = new ArgumentParser({
+//     description: "CLI cross X checkin"
+// });
+// parser.add_argument('-e', '--email', { help: 'Email for login' });
+// parser.add_argument('-p', '--password', { help: 'Password for login' });
+// parser.add_argument('--token', { help: 'Directly use token instead of credentials' });
+// parser.add_argument('-t', '--time', { help: 'Class time format HH:mm', required: true });
 
-const args = parser.parse_args()
+// const args = parser.parse_args()
 
-const {time: targetTime, email, password, token} = args;
+// const {time: targetTime, email, password, token} = args;
 
-if (!targetTime || !/[0-9]{2}:[0-9]{2}/) {
-    console.error("Class time must follow pattern \"HH:mm\"")
-    exit()
-}
+// if (!targetTime || !/[0-9]{2}:[0-9]{2}/) {
+//     console.error("Class time must follow pattern \"HH:mm\"")
+//     exit()
+// }
 
-if (!token && (!email || !password)) {
-    console.error("Either pass auth credentials (email and password) or directly inform auth token")
-    exit()
-}
+// if (!token && (!email || !password)) {
+//     console.error("Either pass auth credentials (email and password) or directly inform auth token")
+//     exit()
+// }
 
-const targetHour = parseInt(targetTime.split(":")[0]);
-const targetMinute = parseInt(targetTime.split(":")[1]);
+// const targetHour = parseInt(targetTime.split(":")[0]);
+// const targetMinute = parseInt(targetTime.split(":")[1]);
 
 
-if (targetHour >= 24 || targetHour < 0) {
-    console.error("Invalid hour")
-}
+// if (targetHour >= 24 || targetHour < 0) {
+//     console.error("Invalid hour")
+// }
 
-if (targetMinute >= 60 || targetMinute < 0) {
-    console.error("Invalid minute")
-}
+// if (targetMinute >= 60 || targetMinute < 0) {
+//     console.error("Invalid minute")
+// }
 
 // (async () => {
 
+//     const crossAPI = new CrossAPI();
 //     if (token) {
-//         setToken(token)
+//         crossAPI.setToken(token)
 //     } else {
-//         await authenticate(email, password)
+//         await crossAPI.authenticate(email, password)
 //     }
-
-//     const classes = await getClassesForDate(dayjs()) 
+    
+//     const classes = await crossAPI.getClassesForDate(dayjs()) 
     
 //     const targetClass = classes.find(classItem => classItem.start_time == targetTime)
+//     console.log(classes)
 //     if (!targetClass) {
 //         console.error("Class not found");
 //         exit();
 //     } else {
-//         console.log("Found class id: " +  targetClass.id)
+//         console.log("Found class id: " +  targetClass?.id)
 //     }
     
 //     let checkinTime = dayjs()
@@ -74,7 +75,7 @@ if (targetMinute >= 60 || targetMinute < 0) {
 //         setTimeout(async () => {        
 //             let attempts = 0;
 //             console.log("attempt")
-//             while(!(success = await checkin(targetClass?.id as number))) {
+//             while(!(success = await crossAPI.checkin(targetClass?.id as number))) {
 //                 attempts++;
 //                 if ( attempts > 5) {
 //                     console.log("Maximum attempts reached: " + attempts)
